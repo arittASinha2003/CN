461d4b1f634c951ab700f7b9fff201539de6002d
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-04-28 13:20:34 +0530

    Add files via upload

---
+++ Exp. 6/Client3.py
@@ -0,0 +1,17 @@
+# Client 3
+
+from socket import *
+host= '127.0.0.1'
+port= 5009
+client_soc= socket(AF_INET, SOCK_DGRAM)
+file_name= input('Enter file name required: ')
+client_soc.sendto(file_name.encode(),(host, port))
+file, server_adrs= client_soc.recvfrom(1024)
+file=file.decode()
+if file==' ':
+    print('Empty file')
+else:
+    print(file)
+print('File received by Client03')
+client_soc.close()
+print('Connection Lost')
