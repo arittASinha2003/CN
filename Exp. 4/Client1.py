bdb3f10320fc6438a20de6458d5983b2639b1922
Aritt Sinha <93367827+arittASinha2003@users.noreply.github.com>
2023-04-28 13:04:56 +0530

    Add files via upload

---
+++ Exp. 4/Client1.py
@@ -0,0 +1,20 @@
+# Client 1 code
+import socket
+client=socket.socket()
+source=input( "Enter desired file: " )
+client.connect(( "localhost" ,5006))
+client.send(source.encode())
+n=input( "Enter new file name: " )
+with open(n, "wb") as f:
+    print( 'File opened' )
+    while True:
+        data=client.recv(1024)
+        if not data:
+            break
+        print( 'Receiving data...' )
+        print('Data in New file:')
+        print(data)
+        print( "file recieved by client01" )
+        print( "Connection Lost" )
+        f.close()
+client.close()
